13cad10bef5cd285abd1d8e3b5aef613d3269f15
jack <jack.giannini@gmail.com>
2025-08-10 12:52:10 -0400

    initial commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Jack4Code/go-sandbox
+
+go 1.24.5
+
+require github.com/Jack4Code/go-internal-tools v1.0.1
---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/Jack4Code/go-internal-tools/flex_capacitor"
+)
+
+func main() {
+	dt := flex_capacitor.GoToFuture(5)
+	fmt.Println(dt)
+}
